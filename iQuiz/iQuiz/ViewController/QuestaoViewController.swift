28df0b415b93b914b9f2da234f575a33cc95f0f6
ArthurLVidal <arthurodri2017@gmail.com>
2025-01-20 22:44:43 -0300

    Finalizando App

--- iQuiz/iQuiz/ViewController/QuestaoViewController.swift
+++ iQuiz/iQuiz/ViewController/QuestaoViewController.swift
@@ -30,8 +30,14 @@ class QuestaoViewController: UIViewController {
         if numeroQuestao < questoes.count - 1 {
             numeroQuestao += 1
             Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(configurarQuestao), userInfo: nil, repeats: false)
+        } else {
+            navegaParaTelaDesempenho()
         }
         
+    }
+    
+    func navegaParaTelaDesempenho() {
+        performSegue(withIdentifier: "irParaTelaDesempenho", sender: nil)
     }
     
     
@@ -59,5 +65,10 @@ class QuestaoViewController: UIViewController {
             botao.setTitle(tituloBotao, for: .normal)
             botao.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let desempenhoVC = segue.destination as? DesempenhoViewController else { return }
+        desempenhoVC.pontuacao = pontuacao
     }
 }
